55f900bc7bb857e64cec4c073a52fdbf27ce3274
Luis Nava <levacarrillo@gmail.com>
2021-06-08 17:30:56 -0500

    First draft

--- app/src/main/java/com/eurosoft/qrreader/MainActivity.kt
+++ app/src/main/java/com/eurosoft/qrreader/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.eurosoft.qrreader
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.eurosoft.qrreader.databinding.ActivityMainBinding
+import com.google.zxing.integration.android.IntentIntegrator
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnScanner.setOnClickListener{ initScanner()}
+    }
+
+    private fun initScanner() {
+        //Toast.makeText( this, "Heeey this is not useful", Toast.LENGTH_SHORT).show()
+        IntentIntegrator(this).initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if(result != null){
+            if(result.contents == null){
+                Toast.makeText(this, "Canceled", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "The values is: ${result.contents}", Toast.LENGTH_SHORT).show()
+            }
+
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+
     }
 }
